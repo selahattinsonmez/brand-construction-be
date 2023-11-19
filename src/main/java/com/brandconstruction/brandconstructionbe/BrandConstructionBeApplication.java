f58ac05019c3f65b2100ddbe432ae49796dfe49c
Selahattin Sönmez <selahattin.sonmez01@hotmail.com>
2023-11-19 17:11:36 +0300

    first implementations

---
+++ src/main/java/com/brandconstruction/brandconstructionbe/BrandConstructionBeApplication.java
@@ -0,0 +1,13 @@
+package com.brandconstruction.brandconstructionbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandConstructionBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrandConstructionBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brandconstruction/brandconstructionbe/BrandConstructionBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brandconstruction.brandconstructionbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandConstructionBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
